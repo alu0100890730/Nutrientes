39ad22bcf36807a2fa905dd657f2c1b51b815899
alu0100890730 <alu0100890730@ull.edu.es>
2017-10-27 11:53:56 +0100

    Añadiendo primer alimento en clase

---
+++ lib/prct06/valor_calorico.rb
@@ -0,0 +1,10 @@
+class Alimento
+  
+  attr_reader :nombre
+    
+  def initialize(nombre)
+    @nombre = nombre
+  end
+  
+  
+end
--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -1,9 +1,16 @@
 require "spec_helper"
 require "./lib/prct06/valor_calorico.rb"
 
-RSpec.describe Alimento do
+RSpec.describe Prct06 do
   it "Tiene una version" do
-    expect(Alimento::VERSION).not_to be nil
+    expect(Prct06::VERSION).not_to be nil
+  end
+end
+
+RSpec.describe Alimento do
+  
+  before :all do
+    @HuevoFrito = Alimento.new("Huevo Frito")
   end
   
   it "Existe nombre para el alimento" do
