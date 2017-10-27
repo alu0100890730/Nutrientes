769f88f641f0f3c776143dc560b2ae77e552e023
alu0100890730 <alu0100890730@ull.edu.es>
2017-10-27 11:43:55 +0100

    Creando alimento

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Prct06 do
   it "has a version number" do
     expect(Prct06::VERSION).not_to be nil
   end
+  
+  it "Existe nombre para el alimento" do
+    expect(@HuevoFrito.nombre).not_to be nil
+  end
+  
+  
 end
