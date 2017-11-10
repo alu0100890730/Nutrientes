f3281ae3faee32d9cc7bb24d3c89aa55186865c5
alu0100890730 <alu0100890730@ull.edu.es>
2017-11-10 12:03:09 +0000

    Terminado

--- lib/prct06/AlimentoC.rb
+++ lib/prct06/AlimentoC.rb
@@ -1,17 +1,15 @@
 #require "valor_calorico.rb"
 
 class AlimentoC < Alimento
-  attr_accessor :nombre, :grupo
+  attr_accessor :grupo
   
   def initialize(nombre, proteinas, glucidos, lipidos, grupo)
-    @nombre = nombre
     super(nombre, proteinas, glucidos, lipidos)
     @grupo = grupo
   end
   
   def to_s
-    s = "#{@nombre}\t"
-    s += super.to_s
+    s = super.to_s
     s += "\t#{@grupo}"
     s
   end
