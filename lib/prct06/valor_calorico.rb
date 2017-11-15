932e00389f6dbcc46d9f9607b97ca00f6314a57c
alu0100890730 <alu0100890730@ull.edu.es>
2017-11-15 13:02:20 +0000

    Creado operador >

--- lib/prct06/valor_calorico.rb
+++ lib/prct06/valor_calorico.rb
@@ -1,13 +1,27 @@
 class Alimento
   
   attr_reader :nombre, :proteinas, :glucidos, :lipidos
-    
+  include Comparable
+  
   def initialize(nombre, proteinas, glucidos, lipidos)
     @nombre = nombre
     @proteinas = proteinas
     @glucidos = glucidos
     @lipidos = lipidos
   end  
+  
+  def <=>(other) #operador !=
+    return nil unless other.is_a?Alimento #devuelve nil si no son de la clase Alimento los dos objetos
+    if valor_calorico == other.valor_calorico
+      return 0
+    else
+      if valor_calorico < other.valor_calorico
+	return -1 #devuelve true cuando es menor, y lo niega para conseguirlo negado, el operador < solo comprueba el menor
+      else
+	return 1 #devuelve true si es >
+      end
+    end
+  end
   
   def to_s
         imprime = "#{@nombre}: #{@proteinas} #{@glucidos} #{@lipidos}"
