45c06d4d2db1363b731e84efb0ab4affce5d471b
alu0100890730 <alu0100890730@ull.edu.es>
2017-11-28 17:42:00 +0000

    Añadido el metodo benchmark

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -2,6 +2,9 @@ require "spec_helper"
 require "./lib/prct06/valor_calorico.rb"
 require "./lib/prct06/lista.rb"
 require "./lib/prct06/AlimentoC.rb"
+require 'benchmark'
+
+include Benchmark
 
 RSpec.describe Prct06 do
   it "Tiene una version" do
@@ -472,4 +475,82 @@ RSpec.describe Alimento do
     expect(igChocolate).to eq(14.029097576530623)
   end 
   
+end
+
+
+Benchmark.benchmark(CAPTION, 7, FORMAT, ">total:", ">avg:") do |x|
+  
+    @HuevoFrito = AlimentoC.new("Huevo Frito", 14.1, 0.0, 19.5, "Huevos, lacteos y helados" )
+    @LecheVaca = AlimentoC.new("Leche vaca", 3.3, 4.8, 3.2, "Huevos, lacteos y helados")
+    @Yogurt = AlimentoC.new("Yogurt", 3.8, 4.9, 3.8, "Huevos, lacteos y helados")
+    @Cerdo = AlimentoC.new("Cerdo", 21.5, 0.0, 6.3, "Carnes y derivados")
+    @Ternera = AlimentoC.new("Ternera", 21.1, 0.0, 3.1, "Carnes y derivados")
+    @Pollo = AlimentoC.new("Pollo", 20.6, 0.0, 5.6, "Carnes y derivados")
+    @Bacalao = AlimentoC.new("Bacalao",  17.7, 0.0, 0.4, "Pescados y mariscos") 
+    @Atun = AlimentoC.new("Atun", 21.5, 0.0, 15.5, "Pescados y mariscos") 
+    @Salmon = AlimentoC.new("Salmon",  19.9, 0.0, 13.6, "Pescados y mariscos") 
+    @AceiteOliva = AlimentoC.new("Aceite de oliva", 0.0, 0.2, 99.6, "Alimentos grasos") 
+    @Mantequilla = AlimentoC.new("Mantequilla", 0.7, 0.0, 83.2, "Alimentos grasos") 
+    @Chocolate = AlimentoC.new("Chocolate", 5.3, 47.0, 30.0, "Alimentos grasos") 
+    @Azucar = AlimentoC.new("Azucar", 0.0, 99.8, 0.0, "Alimentos ricos en carbohidratos") 
+    @Arroz = AlimentoC.new("Arroz",6.8, 77.7, 0.6, "Alimentos ricos en carbohidratos") 
+    @Lentejas = AlimentoC.new("Lentejas",  23.5, 52.0, 1.4, "Alimentos ricos en carbohidratos") 
+    @Papas = AlimentoC.new("Papas",2.0, 15.4, 0.1, "Alimentos ricos en carbohidratos") 
+    @Tomate = AlimentoC.new("Tomate", 1.0, 3.5, 0.2, "Verduras y Hortalizas") 
+    @Cebolla = AlimentoC.new("Cebolla", 1.3, 5.8, 0.3, "Verduras y Hortalizas") 
+    @Calabaza = AlimentoC.new("Calabaza", 1.1, 4.8, 0.1, "Verduras y Hortalizas") 
+    @Manzanas = AlimentoC.new("Manzanas", 0.3, 12.4, 0.4, "Frutas") 
+    @Platanos = AlimentoC.new("Platanos", 1.2, 21.4, 0.2, "Frutas") 
+    @Pera = AlimentoC.new("Pera",  0.5, 12.7, 0.3, "Frutas")
+    
+    @nodo1 = Nodo.new(@HuevoFrito)
+    @nodo2 = Nodo.new(@LecheVaca)
+    @nodo3 = Nodo.new(@Yogurt)
+    @nodo4 = Nodo.new(@Cerdo)
+    @nodo5 = Nodo.new(@Ternera)
+    @nodo6 = Nodo.new(@Pollo)
+    @nodo7 = Nodo.new(@Bacalao)
+    @nodo8 = Nodo.new(@Atun)
+    @nodo9 = Nodo.new(@Salmon)
+    @nodo10 = Nodo.new(@AceiteOliva)
+    @nodo11 = Nodo.new(@Mantequilla)
+    @nodo13 = Nodo.new(@Chocolate)
+    @nodo14 = Nodo.new(@Azucar)
+    @nodo15 = Nodo.new(@Arroz)
+    @nodo16 = Nodo.new(@Lentejas)
+    @nodo17 = Nodo.new(@Papas)
+    @nodo18 = Nodo.new(@Tomate)
+    @nodo19 = Nodo.new(@Cebolla)
+    @nodo20 = Nodo.new(@Calabaza)
+    @nodo21 = Nodo.new(@Manzanas)
+    @nodo22 = Nodo.new(@Platanos)
+    @nodo23 = Nodo.new(@Pera)
+    
+    @lista = Lista.new()
+    @lista.insert_queue(@nodo1)
+    @lista.insert_queue(@nodo2)
+    @lista.insert_queue(@nodo3)
+    @lista.insert_queue(@nodo4)
+    @lista.insert_queue(@nodo5)
+    @lista.insert_queue(@nodo6)
+    @lista.insert_queue(@nodo7)
+    @lista.insert_queue(@nodo8)
+    @lista.insert_queue(@nodo9)
+    @lista.insert_queue(@nodo10)
+    @lista.insert_queue(@nodo11)
+    @lista.insert_queue(@nodo13)
+    @lista.insert_queue(@nodo14)
+    @lista.insert_queue(@nodo15)
+    @lista.insert_queue(@nodo16)
+    @lista.insert_queue(@nodo17)
+    @lista.insert_queue(@nodo18)
+    @lista.insert_queue(@nodo19)
+    @lista.insert_queue(@nodo20)
+    @lista.insert_queue(@nodo21)
+    @lista.insert_queue(@nodo22)
+    @lista.insert_queue(@nodo23)
+
+  tf = x.report("for") {for i in 0..30 do @lista.convertirArray(@lista) end}
+  tf = x.report("each") {for i in 0..30 do @lista.ordenandoEach(@lista) end}
+  tf = x.report("sort") {for i in 0..30 do @lista.ordenandoSort(@lista) end}
 end
