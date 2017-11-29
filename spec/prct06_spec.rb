c87c5f8bdfa0514242f29ed04ae84b65164215a3
alu0100890730 <alu0100890730@ull.edu.es>
2017-11-29 12:51:29 +0000

    Mejorando Coveralls

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -206,6 +206,14 @@ RSpec.describe Lista do
       expect(@lista.tamanio).to eq(4)
   end
   
+  it "Extrayendo nodos de la lista" do
+      @lista.insert_head(@nodo2)
+      
+      @lista.pop_queue
+      expect(@lista.head).to eq(nil)
+      expect(@lista.tamanio).to eq(0)
+  end
+  
   it"Extraer primer elemento" do
     @lista.insert_head(@nodo2)
     @lista.insert_head(@nodo1)
